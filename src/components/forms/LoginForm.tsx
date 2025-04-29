87360d892e124846b7c80e6a985b03aab1ac36a4
Awe-olanrewaju <aweolanrewaju@punch.gency>
2025-04-29 14:44:23 +0100

    Implemented the login flow with the backend and also the fetching of booking data when on the dashboard. I also managed cookies for session control

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -2,18 +2,21 @@ import React, { useState } from "react";
 import { Input, Button, Spinner } from "../shared";
 import { login } from "../../service/auth";
 import { ApiError } from "../../types";
-
+import { setUserCookie } from "../../utils/user";
+import { useNavigate } from "react-router";
 export const LoginForm = () => {
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
-    email: "",
+    username: "",
     password: "",
   });
   const [error, setError] = useState("");
+  const [success, setSuccess] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+    setFormData({ ...formData, [name]: value.trim() });
   };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -22,17 +25,22 @@ export const LoginForm = () => {
     if (error) {
       setError("");
     }
-    if (formData.email === "" || formData.password === "") {
+    if (formData.username === "" || formData.password === "") {
       setError("Please fill in all fields");
       return;
     }
-    console.log(formData);
     try {
       setIsLoading(true);
-      const response = await login(formData.email, formData.password);
-      console.log(response);
+      const response = await login(formData.username, formData.password);
+      if (response.status === 200) {
+        console.log(response.data, "here is the response");
+        setUserCookie(response.data.token);
+        setSuccess(true);
+        navigate("/dashboard");
+      }
     } catch (error) {
       const apiError = error as ApiError;
+      console.log(apiError, "here is the error");
       setError(apiError.response.data.message);
     } finally {
       setIsLoading(false);
@@ -41,12 +49,12 @@ export const LoginForm = () => {
   return (
     <form onSubmit={handleSubmit} className="flex flex-col gap-4">
       <Input
-        label="Email Address"
+        label="Username"
         icon="mail"
-        type="email"
-        name="email"
-        placeholder="Enter your email"
-        value={formData.email}
+        type="text"
+        name="username"
+        placeholder="Enter your username"
+        value={formData.username}
         onChange={handleChange}
       />
 
@@ -63,6 +71,13 @@ export const LoginForm = () => {
         <div className="bg-red-100 rounded-lg px-2 py-3">
           <p className="text-red-500 font-medium text-sm text-center">
             {error}
+          </p>
+        </div>
+      )}
+      {success && (
+        <div className="bg-green-100 rounded-lg px-2 py-3">
+          <p className="text-green-500 font-medium text-sm text-center">
+            Login successful!
           </p>
         </div>
       )}
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,22 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router";
-import { Login } from "./pages";
-
+import { Login, Dashboard } from "./pages";
+import { Home } from "./layout";
 const router = createBrowserRouter([
   {
     path: "/login",
     element: <Login />,
+  },
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      {
+        path: "dashboard",
+        element: <Dashboard />,
+      },
+    ],
   },
 ]);
 createRoot(document.getElementById("root")!).render(
